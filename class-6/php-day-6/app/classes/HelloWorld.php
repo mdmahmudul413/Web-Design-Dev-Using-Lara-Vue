ad95ab0f0c4865206058423fd91de2df412c7c6e
Mahmud <mdmahmudul413@gmail.com>
2022-10-26 09:03:15 +0600

    Class 6 done

--- class-6/php-day-6/app/classes/HelloWorld.php
+++ class-6/php-day-6/app/classes/HelloWorld.php
@@ -5,6 +5,7 @@ class HelloWorld
 {
     public $message;
     public $i;
+    public $data = [];
 
     public function __construct()
     {
@@ -18,8 +19,229 @@ class HelloWorld
 //            echo $this->message; //false condition
 //        }
 
-        for ($this->i = 10; $this->i < 15; $this->i++){
-            echo $this->message . "<br>"; //true condition
+//        for ($this->i = 10; $this->i < 15; $this->i++){
+//            echo $this->message . "<br>"; //true condition
+//        }
+
+//        To print 100 to 200
+//        for ($this->i = 100; $this->i <= 200; $this->i++){
+//            echo $this->i . " ";
+//        }
+
+//        To print 50 to 30
+//        for ($this->i = 50; $this->i >= 30; $this->i--){
+//            echo $this->i . " ";
+//        }
+
+
+////        Repeated statement "for" example
+//        $this->i = 100;
+//        while($this->i > 105){
+//            echo $this->i . " "; // false condition
+//            $this->i++;
+//        }
+
+
+//        $this->i = 100;
+//        while($this->i < 105){
+//            echo $this->i . "<br/>"; // true condition
+//            $this->i++;
+//        }
+
+////        Repeated statement "do while" example
+
+//        $this->i = 10;
+//        do{
+//            echo $this->i . ' ';//false condition but execute at least one time
+//            $this->i++;
+//        }while($this->i > 15);
+
+//        $this->i = 10;
+//        do{
+//            echo $this->i . ' ';//true condition
+//            $this->i++;
+//        }while($this->i < 15);
+
+
+
+//        Example of Array
+
+//        This is not the real practice of array
+//        $this->data = [10, 20, 30, 'BITM', 'BASIS', true, 100.200];
+//        echo $this->data[9];
+
+
+//        Now we will start the original practice array
+
+//        This is not appropriate array
+//        $this->data = ['Habib', 'habib@gmail.com','12345'];
+
+
+
+//        But this is appropriate and meaningful array
+//        suppose, we store a student details
+//        $this->data = [
+//            'name' => 'Habib',
+//            'email' => 'habib@gmail.com',
+//            'phone' => '12345'
+//        ];
+
+
+//        To print a single value
+//        echo $this->data['name'];
+
+
+////        To print all the value
+//        foreach($this->data as $data){
+//            echo $data . '  &nbsp;';
+//        }
+
+//        To print all the key and value
+//        foreach($this->data as $key => $data){
+//            echo $key . ':' . $data . '<br/>';
+//        }
+
+
+
+//        suppose, we store multiple students details
+//        $this->data = [
+//            0 => [
+//                'name' => 'Habib',
+//                'email' => 'habib@gmail.com',
+//                'phone' => '12345'
+//            ],
+//            1 => [
+//                'name' => 'Mahmud',
+//                'email' => 'mahmud@gmail.com',
+//                'phone' => '45254'
+//            ],
+//            2 => [
+//                'name' => 'Riyad',
+//                'email' => 'riyad@gmail.com',
+//                'phone' => '674567'
+//            ]
+//        ];
+
+
+//        To print a single value
+//        echo $this->data[0]['name'] . '<br/>';
+//        echo $this->data[0]['email'] . '<br/>';
+//        echo $this->data[0]['phone'] . '<br/>';
+
+
+//        To print only name
+//        foreach ($this->data as $item){
+//                echo $item['name'] . '<br/>';
+//        }
+
+
+
+//        To print all the value
+//        foreach ($this->data as $key => $value){
+//            echo ($key+1) . '.' . ' ';
+//            foreach ($value as $key1 => $item){
+//                echo $key1 .': '. $item . ' ';
+//            }
+//            echo '<br/>';
+//        }
+
+
+
+//        To print a specific index
+
+//        foreach ($this->data as $key => $value){
+//            if($key == 2){
+//                echo ($key+1) . '.' . ' ';
+//                foreach ($value as $key1 => $item){
+//                    echo $key1 .': '. $item . ' ';
+//                }
+//                echo '<br/>';
+//            }
+//        }
+
+
+
+//        To print a specific index
+
+//        foreach ($this->data as $key => $value){
+//            if($key == 2){
+//                echo ($key+1) . '.' . ' ';
+//                foreach ($value as $key1 => $item){
+//                    echo $key1 .': '. $item . ' ';
+//                }
+//                echo '<br/>';
+//            }
+//        }
+
+
+
+//        suppose, we store multiple students details and take multiple phone number
+
+        $this->data = [
+            0 => [
+                'name' => 'Habib',
+                'email' => 'habib@gmail.com',
+                'phone' => [
+                    'personal' => '12345',
+                    'parent' => '1235'
+                ]
+            ],
+            1 => [
+                'name' => 'Mahmud',
+                'email' => 'mahmud@gmail.com',
+                'phone' => [
+                    'personal' => '6543',
+                    'parent' => '4567'
+                ]
+            ],
+            2 => [
+                'name' => 'Riyad',
+                'email' => 'riyad@gmail.com',
+                'phone' => [
+                    'personal' => '567',
+                    'parent' => '9876'
+                ]
+            ]
+        ];
+
+
+
+//        To print a specific index
+
+        foreach ($this->data as $key => $value){
+            echo ($key+1) . '.' . ' ';
+            foreach ($value as $key1 => $item){
+                if(is_array($item)){
+//                    if we want to print just parent phone number
+//                    echo $item['parent'];
+                    echo $key1 . ':' . '[';
+                    foreach ($item as $key2 => $v_item){
+                        echo $key2 . ':' . $v_item . ' ';
+                    }
+                    echo ']';
+                }
+                else{
+                    echo $key1 .': '. $item . ' ';
+                }
+            }
+            echo '<br/>';
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- class-6/readme.md
+++ class-6/readme.md
@@ -3,5 +3,20 @@
 3. Repeated statement "for" example
 4. Working process of "for" loop
 5. "for" loop is working anti clock wise behavior
+6. Repeated statement "while" example
+7. Ideal practice of writing HTML tag
+8. Characteristics of "do while"
+9. Difference between "for, while" and "do while"
+10. Difference between "for, while, do while" and "foreach"
+11. Array
+12. Example of Array
+13. We use 4 types of data in php
+14. Warning: Array to string conversion
+15. Warning: Undefined array key
+16. Warning: Undefined property
+17. How to decrease memory loss in an array 
+18. Array Object Sign(=>)
+19. The real practice of Array
+
 
 
