edd9fcc74a7cabbd55215a1e6ab1bc68f5dd7d69
Mahmud <mdmahmudul413@gmail.com>
2022-11-07 23:54:00 +0600

    class 10 done

---
+++ class-10/php-day-10/pages/home.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css"></link>
+</head>
+<body>
+    <!--Header Section-->
+    <header class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-2 col-m4 h-200 demo">LOGO</div>
+                <div class="col-10 col-m8 h-200">BANNER</div>
+            </div>
+        </div>
+    </header>
+
+    <!--Menu Section-->
+    <nav class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 h-50">MENU</div>
+            </div>
+        </div>
+    </nav>
+
+    <!--Main Content Section-->
+    <section class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-3 h-600">SIDEBAR</div>
+                <div class="col-9 h-600">MAIN CONTENT</div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="">
+        <section>
+    <!--Big Footer Section-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-3 col-m6 h-250">BIG FOOTER ONE</div>
+                    <div class="col-3 col-m6 h-250">BIG FOOTER TWO</div>
+                    <div class="col-3 col-m6 h-250">BIG FOOTER THREE</div>
+                    <div class="col-3 col-m6 h-250">BIG FOOTER FOUR</div>
+                </div>
+            </div>
+        </section>
+
+        <!--Main Footer Section-->
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 h-50">MAIN FOOTER</div>
+                </div>
+            </div>
+        </section>
+    </footer>
+</body>
+</html>
---
+++ class-10/readme.md
@@ -0,0 +1,19 @@
+## **Class 10**
+
+    1. Industry Certified Page Layout Structure
+    2. If we mistakenly give wrong value to width of the Content Area
+    3. Pseudo Element & Pseudo Class
+    4. Pseudo class, elements writing style in CSS version 2
+    5. Pseudo class, elements writing style in CSS version 3
+    6. Difference between pseudo class and pseudo element
+    7. The work of pseudo element ":after"
+    8. Standard naming
+    9. We should not wrap everything inside a container class
+    10. Every html elements has some default margin and padding
+    11. Use case of class Column, Row, Container
+    12. Present standard of making a page SEO friendly(wrap the sections inside semantic tag)
+    12. We always give background color in the semantic element
+    13. Importance of writing Media Query code
+    14. The rules of responsive in the small device
+    15. max-width, min-width, media query syntax
+    16. media query example
